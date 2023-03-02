5b6f4317f50b54c00668214dbd0580cd36ef6435
Zahid Ali <75961991+zahidali-007@users.noreply.github.com>
2023-03-02 15:15:32 +0500

    Update day11_task3.py

--- 02-03-2023/day11_task3.py
+++ 02-03-2023/day11_task3.py
@@ -7,17 +7,25 @@ UPLOAD_FOLDER = './upload'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         if 'file1' not in request.files:
             return 'there is no file1 in form!'
         file1 = request.files['file1']
+        if not allowed_file(file1.filename):
+            return 'invalid file type!'
         path = os.path.join(app.config['UPLOAD_FOLDER'], file1.filename)
         file1.save(path)
         with open(path, 'rb') as f:
             file_hash = hashlib.md5(f.read()).hexdigest()
-        return file_hash
+        return ("md5 hash of the given image : "+file_hash)
 
     return '''
     <h1>Upload new File</h1>
