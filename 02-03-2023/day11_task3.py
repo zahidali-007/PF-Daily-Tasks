daaaf5657a450e07a8782640f47401ea2970921c
Zahid Ali <75961991+zahidali-007@users.noreply.github.com>
2023-03-02 15:06:27 +0500

    Create day11_task3.py

---
+++ 02-03-2023/day11_task3.py
@@ -0,0 +1,28 @@
+import hashlib
+import os
+from flask import Flask, request
+
+UPLOAD_FOLDER = './upload'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file1' not in request.files:
+            return 'there is no file1 in form!'
+        file1 = request.files['file1']
+        path = os.path.join(app.config['UPLOAD_FOLDER'], file1.filename)
+        file1.save(path)
+        with open(path, 'rb') as f:
+            file_hash = hashlib.md5(f.read()).hexdigest()
+        return file_hash
+
+    return '''
+    <h1>Upload new File</h1>
+    <form method="post" enctype="multipart/form-data">
+      <input type="file" name="file1">
+      <input type="submit">
+    </form>
+    '''
