9e832bb961117aa8a0c4fafa1f0ec58bbd6fc6ca
Zahid Ali <75961991+zahidali-007@users.noreply.github.com>
2023-03-10 15:39:55 +0500

    Update day7_task3.py

--- 24-02-2023/day7_task3.py
+++ 24-02-2023/day7_task3.py
@@ -12,5 +12,5 @@ def add_to_list(value, lst):
 
 my_list = [random.randint(0, 10) for _ in range(10)]
 print(f"Initial list: {my_list}")
-add_to_list(2, my_list)  # Value '2' already exists in the list.
+add_to_list(2, my_list)  # Value '2' already exists in the list
 add_to_list(7, my_list)  # Added '7' to the list. List up until '7': [6, 2, 1, 0, 6, 2, 7]
