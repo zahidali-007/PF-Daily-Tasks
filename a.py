510b3c2bfb7c346d45c7c0c0c42bca6911117ee5
Zahid <zahidmahar008@gmail.com>
2023-03-14 13:03:11 +0500

    intial commit

--- a.py
+++ a.py
@@ -1,2 +1,3 @@
 print("First commit using git commands")
 print("Main branch")
+print("Checking on my account commit")
---
+++ bashscripts/a.py
@@ -0,0 +1,4 @@
+print("heloo from git command")
+print('Checking before commit')
+print("I'm from bash1 branch")
+print("After uploading")
